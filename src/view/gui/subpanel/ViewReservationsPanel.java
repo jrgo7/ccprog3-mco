779961f34c1cee841fe9ea960749d926ac779b7b
WataRain <81730861+WataRain@users.noreply.github.com>
2024-07-26 13:08:28 +0800

    Initially hide reservation list panels

--- src/view/gui/subpanel/ViewReservationsPanel.java
+++ src/view/gui/subpanel/ViewReservationsPanel.java
@@ -20,8 +20,6 @@ public class ViewReservationsPanel extends StyledPanel {
     public ViewReservationsPanel() {
         this.setLayout(new BorderLayout());
 
-        reservationDataPanel = new StyledPanel();
-        reservationDataPanel.setLayout(new BorderLayout());
 
         this.outer = new StyledPanel();
         this.outer.setLayout(new BoxLayout(outer, BoxLayout.Y_AXIS));
@@ -29,13 +27,13 @@ public class ViewReservationsPanel extends StyledPanel {
         this.reservationListPanel = new ReservationListPanel(200);
         this.reservationDataComponent = new StyledHTMLPane();
 
-        StyledPanel scrolLPaneContainer = StyledHTMLPaneContainerFactory
+        reservationDataPanel = StyledHTMLPaneContainerFactory
                 .createContainer(reservationDataComponent);
-        this.outer.add(scrolLPaneContainer);
+        this.outer.add(reservationDataPanel);
 
         this.add(this.reservationListPanel, BorderLayout.WEST);
         this.add(outer, BorderLayout.CENTER);
-
+        this.setWrapperVisible(false);
     }
 
     public void setReservationList(ArrayList<String> data) {
